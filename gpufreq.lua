2c54cf9564337551be5ec99257663c07ab099427
Nicolas F <ovdev@fratti.ch>
2020-04-04 23:55:32 +0200

    Add AMD memory clocks

--- gpufreq.lua
+++ gpufreq.lua
@@ -24,8 +24,8 @@ function show_gpu_freq(card)
         mp.osd_message("Could not identify GPU vendor for card " .. card)
         return
     end
-    local freq_cur, freq_max, freq_req = freqgetter[vendor](freqgetter, card)
-    display_gpu_freq(freq_cur, freq_max, freq_req)
+    local frequencies = freqgetter[vendor](freqgetter, card)
+    display_gpu_freq(frequencies)
 end
 
 function card_path(card)
@@ -49,14 +49,16 @@ function freqgetter:intel(card)
     local freq_cur = get_sys_value(card_path(card) .. "gt_act_freq_mhz")
     local freq_max = get_sys_value(card_path(card) .. "gt_max_freq_mhz")
     local freq_req = get_sys_value(card_path(card) .. "gt_cur_freq_mhz")
-    return freq_cur, freq_max, freq_req
+    return {["core"] = {cur = freq_cur, max = freq_max, req = freq_req}}
 end
 
-function freqgetter:amd(card)
-    local scuffed = get_sys_value(card_path(card) .. "device/pp_dpm_sclk")
+--[[ YOU HAVE ENTERED THE CURSED AMD ZONE ]]--
+
+-- Returns the parsed frequencies from an AMD card
+function amd_pp_consumer(val)
     local fmax = 0
     local fcur = 0
-    for line in spliterator(scuffed) do
+    for line in spliterator(val) do
         local freq = tonumber(line:match("%d: (%d+)Mhz"))
         local is_cur = line:match("%*")
         if is_cur ~= nil then
@@ -66,15 +68,35 @@ function freqgetter:amd(card)
             fmax = freq
         end
     end
-    return fcur, fmax, nil
+    return fcur, fmax
 end
 
-function display_gpu_freq(freq_cur, freq_max, freq_req)
-    if freq_req == nil then
-        mp.osd_message(freq_cur .. "/" .. freq_max .. " MHz")
-    else
-        mp.osd_message(freq_cur .. "/" .. freq_max .. " MHz (requested " .. freq_req .. " MHz)")
+function freqgetter:amd(card)
+    local scuffed = get_sys_value(card_path(card) .. "device/pp_dpm_sclk")
+    local scuffedmem = get_sys_value(card_path(card) .. "device/pp_dpm_mclk")
+    --local core_cur, core_max = amd_pp_consumer(scuffed)
+    local cur, max = amd_pp_consumer(scuffed)
+    local core = {["cur"] = cur, ["max"] = max}
+    local cur_mem, max_mem = amd_pp_consumer(scuffedmem)
+    local mem = {["cur"] = cur_mem, ["max"] = max_mem}
+    return {["core"] = core, ["mem"] = mem}
+end
+
+--[[ YOU ARE NOW LEAVING THE CURSED AMD ZONE ]]--
+
+function display_gpu_freq(freqs)
+    assert(freqs["core"])
+    local cfreqs = freqs["core"]
+    local msg = "Core: "
+        msg = msg .. cfreqs["cur"] .. "/" .. cfreqs["max"] .. " MHz"
+    if cfreqs["req"] then
+        msg = msg .. " (requested " .. cfreqs["req"] .. " MHz)"
     end
+    if freqs["mem"] ~= nil then
+        msg = msg .. "\n"
+        msg = msg .. "Memory: " .. freqs["mem"]["cur"] .. "/" .. freqs["mem"]["max"] .. " MHz"
+    end
+    mp.osd_message(msg)
 end
 
 mp.register_script_message("show-gpu-freq", show_gpu_freq)
